c94bb4119175e44493e37939d9d65b8166298949
Shahram M. Hassan <shahram.mebashar@gmail.com>
2023-05-01 22:29:09 +0300

    remove unnecessary dependencies

--- resources/views/pages/dashboard.vue
+++ resources/views/pages/dashboard.vue
@@ -1,18 +1,6 @@
 <script setup lang="ts">
-import { ref } from "vue"
 import { Head } from "@inertiajs/vue3"
 import AuthLayout from "@/views/layouts/auth/auth.vue"
-import VueTailwindDatepicker from "vue-tailwind-datepicker"
-type datet =
-    | [Date, Date]
-    | { start: Date | string; end: Date | string }
-    | {
-          startDate: Date | string
-          endDate: Date | string
-      }
-    | string
-
-const dateValue = ref<datet>([] as any)
 
 defineLayout(AuthLayout)
 </script>
@@ -22,7 +10,5 @@ defineLayout(AuthLayout)
 
     <div class="mx-auto max-w-4xl p-6 lg:p-8">
         My Date
-        <br />
-        <VueTailwindDatepicker v-model="dateValue" />
     </div>
 </template>
